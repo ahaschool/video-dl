98c31a03cab954bc31d2140d109da0bf2844765c
zfm <1306003944@qq.com>
2018-12-06 14:54:24 +0800

    ahaschool video dl

---
+++ src/Youtube/Channel.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: zfm
+ * Date: 2018/12/3
+ * Time: 3:57 PM
+ */
+namespace Ahaschool\Videodl\Youtube;
+
+use GuzzleHttp\Client;
+
+class Channel {
+
+    /**
+     * 定义基本地址
+     */
+    const BASE_URL = 'https://www.youtube.com/';
+
+    /**
+     * http 请求类
+     * @var null
+     */
+    private $http = null;
+
+    /**
+     * video items
+     * @var array
+     */
+    private $items = [];
+
+    /**
+     * 频道
+     * @var string
+     */
+    private $channel = '';
+
+    /**
+     * Channel constructor.
+     * @param $channel
+     */
+    public function __construct($channel)
+    {
+        $this->channel = $channel;
+    }
+
+    /**
+     * 获取视频条目
+     * @return array
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * 抓取视频信息
+     * @return $this
+     */
+    public function initVideoItems()
+    {
+        $this->http = new Client([
+            'timeout' => 10,
+            'headers' => [
+                'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+                'x-youtube-client-name' => '1',
+                'x-youtube-client-version' => '2.20171011',
+            ]
+        ]);
+        $this->loopGetVideoData();
+        return $this;
+    }
+
+    /**
+     * 保存数据
+     */
+    public function saveItemsToDB()
+    {
+        $pdo = new \PDO('mysql:host=test3;dbname=vdl', 'hjm_dev', 'hjm_dev');
+        $stmt = $pdo->prepare("SELECT * FROM `channel` WHERE `url` = :url");
+        $stmt->execute([':url' => $this->channel]);
+        if (!$channel = $stmt->fetch()) {
+            $stmt = $pdo->prepare("INSERT INTO `channel` (`title`,`url`) VALUES (:title,:url)");
+            $stmt->execute([':title' => 'testtest', ':url' => $this->channel]);
+            $stmt = $pdo->prepare("SELECT * FROM `channel` WHERE `url` = :url");
+            $stmt->execute([':url' => $this->channel]);
+            $channel = $stmt->fetch();
+        }
+        $stmt = $pdo->prepare("DELETE FROM `video` WHERE `channel_id` = :channel_id");
+        $stmt->execute([':channel_id' => $channel['id']]);
+        foreach ($this->items as $item) {
+            $stmt = $pdo->prepare("INSERT INTO `video` (`channel_id`,`video_url`,`title`,`description`,`view_count`,`like_count`,`dislike_count`,`file_name`,`publish_at`) VALUES (:channel_id,:video_url,:title,:description,:view_count,:like_count,:dislike_count,:file_name,:publish_at)");
+            $stmt->execute([
+                ':channel_id' => $channel['id'],
+                ':video_url' => $item,
+                ':title' => '',
+                ':description' => '',
+                ':view_count' => 0,
+                ':like_count' => 0,
+                ':dislike_count' => 0,
+                ':file_name' => '',
+                ':publish_at' => '',
+            ]);
+        }
+        $pdo = null;
+    }
+
+    /**
+     * 循环获取视频地址
+     * @param bool $is_ajax
+     * @param string $url
+     */
+    private function loopGetVideoData($is_ajax = false, $url = '')
+    {
+        if (!$is_ajax) {
+            $res = $this->http->get(self::BASE_URL . $this->channel);
+            $content = $res->getBody()->getContents();
+            preg_match('/gridRenderer\":\{\"items\":(.*?),\"continuations/', $content, $match);
+            $items = isset($match[1]) ? json_decode($match[1], true) : [];
+            array_map(function ($item) {
+                array_push($this->items, $item['gridVideoRenderer']['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']);
+            }, $items);
+            preg_match('/"continuations":(.*?),"trackingParams"/', $content, $next);
+            if (isset($next[1])) {
+                $arr = json_decode($next[1], true);
+                $ctoken = $arr[0]['nextContinuationData']['continuation'];
+                $itct = $arr[0]['nextContinuationData']['clickTrackingParams'];
+                $url = self::BASE_URL . 'browse_ajax?ctoken=' . $ctoken . '&itct=' . $itct;
+                $this->loopGetVideoData(true, $url);
+            }
+        } else {
+            $res = $this->http->get($url);
+            $video = \GuzzleHttp\json_decode($res->getBody(), true);
+            $arr = $video[1]['response']['continuationContents']['gridContinuation'] ?? [];
+            $next = $arr['continuations'][0]['nextContinuationData'] ?? [];
+            if (!empty($arr['items']) && !empty($next)) {
+                array_map(function ($item) {
+                    array_push($this->items, $item['gridVideoRenderer']['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']);
+                }, $arr['items']);
+                $ctoken = $next['continuation'] ?? '';
+                $itct = $next['clickTrackingParams'] ?? '';
+                $url = self::BASE_URL . 'browse_ajax?ctoken=' . $ctoken . '&itct=' . $itct;
+                $this->loopGetVideoData(true, $url);
+            }
+        }
+    }
+
+
+}
---
+++ src/Youtube/Download.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: zfm
+ * Date: 2018/12/4
+ * Time: 11:52 AM
+ */
+
+namespace Ahaschool\Videodl\Youtube;
+
+use YoutubeDl\YoutubeDl;
+use YoutubeDl\Exception\CopyrightException;
+use YoutubeDl\Exception\NotFoundException;
+use YoutubeDl\Exception\PrivateVideoException;
+
+class Download
+{
+    /**
+     * YouTubeDL cmd path
+     */
+    const YTB_DL_CMD_PATH = __DIR__.'/../../bin/youtube-dl';
+
+    /**
+     * YouTube下载对象
+     * @var null|YoutubeDl
+     */
+    private $YoutubeDl = null;
+
+    /**
+     * 初始化函数
+     * Download constructor.
+     * @param $path
+     */
+    public function __construct($path)
+    {
+        $this->YoutubeDl = new YoutubeDl([
+            'continue' => true, // force resume of partially downloaded files. By default, youtube-dl will resume downloads if possible.
+            'format' => 'bestvideo',
+        ]);
+        $this->YoutubeDl->setBinPath(self::YTB_DL_CMD_PATH);
+        $this->YoutubeDl->setDownloadPath($path);
+    }
+
+    /**
+     * @param $key
+     * @return \YoutubeDl\Entity\Video|\YoutubeDl\Entity\Video[]
+     * @throws \Exception
+     */
+    public function dl($key)
+    {
+        if (!$key) {
+            throw new \Exception('key not allow null');
+        }
+        try {
+            return $this->YoutubeDl->download("https://www.youtube.com/watch?v={$key}");
+            // $video->getFile(); // \SplFileInfo instance of downloaded file
+        } catch (NotFoundException $e) {
+            throw new \Exception($e->getMessage());
+            // Video not found
+        } catch (PrivateVideoException $e) {
+            throw new \Exception($e->getMessage());
+            // Video is private
+        } catch (CopyrightException $e) {
+            throw new \Exception($e->getMessage());
+            // The YouTube account associated with this video has been terminated due to multiple third-party notifications of copyright infringement
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+            // Failed to download
+        }
+    }
+}
